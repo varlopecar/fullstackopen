dffdbd7399dc6091924c5dbe9af3789dee01bfb5
Carlos Vargas <varlopecar@gmail.com>
2022-01-03 19:07:35 -0600

    Exercise 1.1 completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header'
+import Content from './Content'
+import Total from './Total'
 
-function App() {
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const result = exercises1 + exercises2 + exercises3
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total result={result} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ part1/courseinfo/src/Total.js
@@ -0,0 +1,5 @@
+const Total = (props) => {
+    return <p>Number of exercises {props.result}</p>
+}
+
+export default Total
